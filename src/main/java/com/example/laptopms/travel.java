11e3c195be2c33814e6539a12e4fe1d4fcf41e79
Moin Shaikh <moin241397@gmail.comm>
2024-03-08 22:55:09 +0530

    hello

---
+++ src/main/java/com/example/laptopms/travel.java
@@ -0,0 +1,10 @@
+package com.example.laptopms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class travel {
+    @GetMapping("/travel")
+    public String getData() {return "I love travelling all over world";}
+}
