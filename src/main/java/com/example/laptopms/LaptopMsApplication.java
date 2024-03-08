149c7263b40bb978340ec65078b0d5bbc7319de3
Moin Shaikh <moin241397@gmail.comm>
2024-03-08 22:49:12 +0530

    hello

---
+++ src/main/java/com/example/laptopms/LaptopMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.laptopms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/laptopms/LaptopMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laptopms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
