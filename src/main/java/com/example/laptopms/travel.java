beba754aa4cea0dbddbc0993203bafebe141178b
Moin Shaikh <moin241397@gmail.comm>
2024-03-14 14:12:15 +0530

    hello moin

--- src/main/java/com/example/laptopms/travel.java
+++ src/main/java/com/example/laptopms/travel.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class travel {
     @GetMapping("/travel")
-    public String getData() {return "I love to travelling all over world";}
+    public String getData() {return "I love to travelling all over worlds";}
 }
